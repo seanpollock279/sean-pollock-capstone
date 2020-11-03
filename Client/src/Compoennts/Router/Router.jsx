6221b1352f0c4a6dbba981860fa11f2550060a4d
Sean Pollock <seanpollock@Seans-MacBook-Air-2.local>
2020-11-03 15:46:25 -0500

    slowly working on sign up and log in pages

--- Client/src/Compoennts/Router/Router.jsx
+++ Client/src/Compoennts/Router/Router.jsx
@@ -5,6 +5,7 @@ import MapPage from '../MapPage/MapPage';
 import Uploader from '../Uploader/Uploader';
 import About from '../About/About';
 import Location from '../Location/Location';
+import SignUp from '../SignUp/SignUp';
 
 function Router (props) {
     return (
@@ -13,6 +14,7 @@ function Router (props) {
                 <Route exact path="/" render={() => <LandingPage/> }/>
                 <Route exact path="/locations" render={() => <MapPage state={props.state} /> }/>
                 <Route path="/about" render={() => <About /> }/>
+                <Route path="/signup" component={SignUp} />
                 <Route path="/upload" render={() => <Uploader addLocation={props.addLocation} onChange={props.onChange} newLocation={props.state.newLocation}/> }/>
                 <Route path="/locations/:id" render={(props) => <Location {...props}/> } />
             </Switch>
