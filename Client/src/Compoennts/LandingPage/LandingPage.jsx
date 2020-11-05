66cf9a9a0c0f7c4cb73314838d2def86a6c1d457
Sean Pollock <seanpollock@Seans-MacBook-Air-2.local>
2020-11-05 17:16:05 -0500

    cleaned up some ui on ladning page, sorting is all written but need to fix one bug with find

--- Client/src/Compoennts/LandingPage/LandingPage.jsx
+++ Client/src/Compoennts/LandingPage/LandingPage.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import Logo from '../../Assets/Logo/OriginalLogo.png';
 import '../LandingPage/landingpage.scss';
+import Toronto from '../../Assets/Imgs/berkay-gumustekin-hRg1KL4-AUE-unsplash.jpg';
+import Montreal from '../../Assets/Imgs/daniel-baylis-WiMzEOsQNoc-unsplash.jpg';
+import Calgary from '../../Assets/Imgs/kyler-nixon-_ZBekGTBh-c-unsplash.jpg';
+import Vancouver from '../../Assets/Imgs/lee-robinson-MzCeUhY3Xy0-unsplash.jpg';
 
 function LandingPage () {
     return (
@@ -13,16 +17,27 @@ function LandingPage () {
                     <p className="landing__description">Looking for a spot for your next headshot session? Struggling to find a scenic location to photograph a bridal party?
                         Start here by searching by location, event or best time of day to shoot.
                     </p>
-                    {/* <input className="landing__search" type="text"/>
-                    <button className="landing__searchBtn">Seach</button> */}
                 </div>
                 <div className="landing__bottomContainer">
                     <h2 className="landing__quickMenu">Quick Glance</h2>
-                    <Link to="/locations"><button className="landing__optionBtn">Toronto</button></Link>
-                    <button className="landing__optionBtn">Montreal</button>
-                    <button className="landing__optionBtn">Calgary</button>
-                    <button className="landing__optionBtn">Vancouver</button>
+                        <Link className="locationLink" to="/locations"><div className="imgTest">
+                            <img className="imgTest__test" src={Toronto} alt="berkay-gumustekin-hRg1KL4-AUE-unsplash"/>
+                            <button className="imgTest__optionBtn">Toronto</button>
+                        </div></Link>
+                        <Link className="locationLink" to="/locations"><div className="imgTest">
+                            <img className="imgTest__test" src={Montreal} alt="daniel-baylis-WiMzEOsQNoc-unsplash"/>
+                            <button className="imgTest__optionBtn">Montreal</button>
+                        </div></Link>
+                        <Link className="locationLink" to="/locations"><div className="imgTest">
+                            <img className="imgTest__test" src={Calgary} alt="kyler-nixon-_ZBekGTBh-c-unsplash.jpg"/>
+                            <button className="imgTest__optionBtn">Calgary</button>
+                        </div></Link>
+                        <Link className="locationLink" to="/locations"><div className="imgTest">
+                            <img className="imgTest__test" src={Vancouver} alt="lee-robinson-MzCeUhY3Xy0-unsplash"/>
+                            <button className="imgTest__optionBtn">Vancouver</button>
+                        </div></Link>
                 </div>
+                
             </div>
             <div className="foot">
 
