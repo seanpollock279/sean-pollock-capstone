9754d4605089fe594f789d3c1e3d0b423b177ee3
Sean Pollock <seanpollock@Seans-MacBook-Air-2.local>
2020-11-05 15:11:00 -0500

    sorting functionality is almost working

--- Client/src/Compoennts/MapPage/MapPage.jsx
+++ Client/src/Compoennts/MapPage/MapPage.jsx
@@ -1,18 +1,96 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LocationCard from '../LocationCard/LocationCard';
 import Header from '../Header/Header';
 import axios from 'axios';
 import './mapPage.scss';
 import SortBar from '../SortBar/SortBar';
+import LocationFoot from '../LocationFoot/LocationFoot';
 
 function MapPage (props) {
     let location = props.state.locations.locations;
+
+    const [sorted, setSorted] = useState();
+
+    const [portraitList, setPortraitList] = useState();
+    const sortPortraits = (event) => {
+        console.log('fired')
+        if (props.state.locations !== undefined) {
+            let newPortraitList = props.state.locations.filter(props.state.locations.categories.find('portraits'))
+            setPortraitList(newPortraitList);
+            setSorted('portraits');
+        }
+    }
+
+    const [weddingList, setWeddingList] = useState();
+    const sortWeddings = (event) => {
+        console.log('fired')
+        if (props.state.locations !== undefined) {
+            let newWeddingList = props.state.locations.filter(props.state.locations.categories.find('wedding'))
+            setPortraitList(newWeddingList);
+            setSorted('wedding');
+        }
+    }
+
+    const [lifestyleList, setLifestyleList] = useState();
+    const sortLifestyle = (event) => {
+        console.log('fired')
+        if (props.state.locations !== undefined) {
+            let newLifestyleList = props.state.locations.filter(props.state.locations.categories.find('lifestyle'))
+            setLifestyleList(newLifestyleList);
+            setSorted('lifestyle');
+        }
+    }
+
+    const [engagementList, setEngagementList] = useState();
+    const sortEngagements = (event) => {
+        console.log('fired')
+        if (props.state.locations !== undefined) {
+            let newEngagementList = props.state.locations.filter(props.state.locations.categories.find('engagements'))
+            setPortraitList(newEngagementList);
+            setSorted('engagements');
+        }
+    }
+
     return (
         <>
             <Header />
-            <SortBar />
+            <SortBar sortPortraits={sortPortraits} sortEngagements={sortEngagements} sortLifestyle={sortLifestyle} sortWeddings={sortWeddings}/>
             <div className="Cards">
-                {location.map(location => 
+                {/* {location.map(location => 
+                    <LocationCard 
+                    location={location.location}
+                    description={location.description}
+                    img={location.img}
+                    id={location.id}
+                    key={location.id}
+                    />
+                )} */}
+            
+                {sorted === 'portraits' ? portraitList.map(item => 
+                <LocationCard 
+                location={item.location}
+                description={item.description}
+                img={item.img}
+                id={item.id}
+                key={item.id} />) : sorted === 'wedding' ? weddingList.map(item => 
+                <LocationCard 
+                location={item.location}
+                description={item.description}
+                img={item.img}
+                id={item.id}
+                key={item.id} />) : sorted === 'lifestyle' ? lifestyleList.map(item => 
+                <LocationCard 
+                location={item.location}
+                description={item.description}
+                img={item.img}
+                id={item.id}
+                key={item.id} />) : sorted === 'engagements' ? engagementList.map(item => 
+                <LocationCard 
+                location={item.location}
+                description={item.description}
+                img={item.img}
+                id={item.id}
+                key={item.id} />) : location.map(location => 
                     <LocationCard 
                     location={location.location}
                     description={location.description}
@@ -22,6 +100,7 @@ function MapPage (props) {
                     />
                 )}
             </div>
+            <LocationFoot />
         </>
     )
 }
--- Client/src/Compoennts/SortBar/SortBar.jsx
+++ Client/src/Compoennts/SortBar/SortBar.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import './sortbar.scss'
 
-export default function SortBar() {
+export default function SortBar(props) {
     return (
         <div className="infoBar">
             <h4 className="infoBar__title">Sort by: </h4>
-            <select className="infoBar__select" name="shoot" id="shoot">
-                <option className="infoBar__option" value="portraits">Portraits</option>
-                <option className="infoBar__option" value="lifestyle">Lifestyle</option>
-                <option className="infoBar__option" value="wedding">Wedding</option>
-                <option className="infoBar__option" value="engagements">Engagements</option>
-            </select>
+                <button className="infoBar__select" onClick={props.sortPortraits} value="portraits">Portraits</button>
+                <button className="infoBar__select" onClick={props.sortLifestyle} value="lifestyle">Lifestyle</button>
+                <button className="infoBar__select" onClick={props.sortWeddings} value="wedding">Wedding</button>
+                <button className="infoBar__select" onClick={props.sortEngagements} value="engagements">Engagements</button>
         </div>
     )
 }
